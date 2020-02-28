a743246f55a71707f16cf47035071be5ef7db11d
hugoim <53946680+hugoim@users.noreply.github.com>
2020-02-27 22:07:20 -0300

    Principal.java
    Principal.java

---
+++ Fontes/Exercicio10.java
@@ -0,0 +1,21 @@
+import java.util.Random;
+
+public class Exercicio10 {
+
+	public static void main(String[] args) {
+		Random gerador = new Random();
+		int[] x = new int[10];
+				
+		//Gerar números
+			for(int i = 0; i > x.length; i++) {
+				x[i] = gerador.nextInt(50);
+				System.out.println(x[i]);
+				
+				while(x[i])
+					
+				}
+			}
+
+	}
+
+}
